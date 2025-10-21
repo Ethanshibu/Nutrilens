44644d83bb9dd66573e353a45ac9d39f7d652271
sanislearning <sanjaybiju10@gmail.com>
2025-10-22 01:32:35 +0530

    Created barebones of the homepage, signin, signup, added cors

--- backend/src/app.py
+++ backend/src/app.py
@@ -1,8 +1,19 @@
 from fastapi import FastAPI
 from fastapi import APIRouter
 from routers import auth #imports the authentication routes
+from fastapi.middleware.cors import CORSMiddleware
 
 app=FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_orgins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
+
+
 router =APIRouter()
 app.include_router(auth.router)
 
---
+++ frontend/src/pages/signup.jsx
@@ -0,0 +1,8 @@
+export default function SignUp() {
+  return (
+    <div>
+      <h1>Sign Up</h1>
+      <p>This will be your registration page.</p>
+    </div>
+  );
+}
